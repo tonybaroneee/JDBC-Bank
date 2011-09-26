8194050421ffdeb534e24aec5db64e9d5ba4c806
Anthony Barone <tonybaroneee@gmail.com>
2011-09-26 01:11:57 -0400

    Successfully printing out a list of all accounts, displaying the account number, account balance, and account owner(s).

--- src/bank/main/BankMain.java
+++ src/bank/main/BankMain.java
@@ -2,15 +2,20 @@ package bank.main;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 /**
  * @author acb1485
  * @version 1.0
  */
 public class BankMain {
+	
+	private static final NumberFormat usFormat = NumberFormat.getCurrencyInstance( Locale.US );  
 
 	// Initialize MySQL DB information into class variables.
 	private static final String url = "jdbc:mysql://mentok.se.rit.edu:3306/acb1485";
@@ -26,7 +31,15 @@ public class BankMain {
 		// Initialize our JDBC variables.
 		Connection con = null;
 		Statement st = null;
+		PreparedStatement pst = null;
 		ResultSet rs = null;
+		ResultSet rs2 = null;
+		
+		// Specify the main queries used in our reporting mechanism.
+		String fetchAllAccountsQuery = "SELECT * FROM CheckingAccount";
+		String fetchAllOwnersForAccount = "SELECT * FROM AccountOwnerAccount, AccountOwner " +
+				"WHERE AccountOwnerAccount.CheckingAccountID = ? " +
+				"AND AccountOwnerAccount.AccountOwnerID = AccountOwner.AccountOwnerID";
 
 		try {
 			// Make the initial connection with our database.
@@ -36,13 +49,33 @@ public class BankMain {
 
 			// Construct our query then execute it, storing the ResultSet.
 			st = con.createStatement();
-			rs = st.executeQuery( "SELECT * FROM pet" );
+			rs = st.executeQuery( fetchAllAccountsQuery );
 
 			// Print out the resulting rows.
-			while( rs.next() ) {
-				System.out.println( rs.getString( "name" ) + ", " + rs.getString( "owner" ) + ", " +
-						rs.getString( "species" ) + ", " + rs.getString( "sec" ) + ", " +
-						rs.getString( "birth" ) + ", " + rs.getString( "death" ) + "." );
+			while ( rs.next() ) {
+				System.out.print( "Account number: " + rs.getInt( 2 ) + 
+						" | Balance: " + usFormat.format( rs.getBigDecimal( 3 ) ) + 
+						" | Owners: " );
+				
+				// Find out who owns this particular checking account.
+				rs2 = null;
+				pst = con.prepareStatement( fetchAllOwnersForAccount );
+				pst.setInt( 1, rs.getInt(1) );
+				rs2 = pst.executeQuery();
+				boolean firstName = true;
+				if ( rs2.next() ) {
+					do {
+						if ( !firstName ) {
+							System.out.print( ", " );
+						} else {
+							firstName = false;
+						}
+						System.out.print( rs2.getString( 5 ) + " " + rs2.getString( 6 ) );
+					} while ( rs2.next() );
+				}
+				
+				System.out.println();
+				
 			}
 
 		} catch ( Exception e ) {
@@ -53,8 +86,14 @@ public class BankMain {
 				if ( rs != null ) {
 					rs.close();
 				}
+				if ( rs2 != null ) {
+					rs2.close();
+				}
 				if ( st != null ) {
 					st.close();
+				}
+				if ( pst != null ) {
+					pst.close();
 				}
 				if ( con != null ) {
 					con.close();
